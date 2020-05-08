64e72c17a6819253ec1a1201ee78fb7b77c54ad1
Nick Chen <chenyahua2012@hotmail.com>
2020-05-08 16:41:37 -0400

    json file created

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const express = require('express');
 const app = express();
 const port = 4000;
 
-
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
 app.get('/', (req, res)=>{
     res.json('hello!')
 })
